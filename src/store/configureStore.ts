1f557948d62a684504365059c79ca9b3326d62e8
firsttris <tristanteufel@googlemail.com>
2017-07-01 20:41:04 +0200

    add redux-dev-tools to store

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,11 +1,14 @@
-import {createStore, applyMiddleware } from 'redux'
+import {createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers/index'
 import thunk from 'redux-thunk';
+
+declare let window: any;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState:any) => createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default configureStore
